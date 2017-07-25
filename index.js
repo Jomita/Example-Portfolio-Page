6be9ffd5c498e23490985669038a7ec59f1cac6b
Joma <jothom42@gmail.com>
2017-07-25 19:02:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+$('.menu-toggle').click(function() {
+  
+  $('.site-nav').toggleClass('site-nav--open');
+  $(this).toggleClass('open');
+  
+})
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("goToTop").style.display = "block";
+    } else {
+        document.getElementById("goToTop").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
+
